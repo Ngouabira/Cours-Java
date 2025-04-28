00bea5d55a7f30aadf9dba5e2ac6040e0a2ad5e3
Valdy NGOUABIRA <valdyngouabira@gmail.com>
2025-04-28 13:43:52 +0000

    utlisation de la classe Math

---
+++ src/Maths.java
@@ -0,0 +1,15 @@
+public class Maths {
+
+    public static void main(String[] args) {
+
+        int a = -5;
+
+        System.out.println(Math.abs(a));
+        System.out.println(Math.max(3, 7));
+        System.out.println(Math.min(3, 7));
+        System.out.println(Math.pow(5, 2));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.PI);
+
+    }
+}
